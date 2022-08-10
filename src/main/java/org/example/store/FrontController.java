490b136775928a46aaccaff8b8267f3b07c548d0
19187978 <X45500554x>
2022-08-10 19:47:58 +0300

    Initial commit

---
+++ src/main/java/org/example/store/FrontController.java
@@ -0,0 +1,24 @@
+package org.example.store;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FrontController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        try {
+            writer.println("<h2>Серверы приложений</h2>");
+        } finally {
+            writer.close();
+        }
+
+    }
+}
